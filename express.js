4a15fb1738f8e1b09c58c9e9efb4e81730b9a321
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-15 14:06:04 -0300

    express get and post diference

--- express.js
+++ express.js
@@ -1,1 +1,22 @@
 let express = require("express")
+let ourApp = express()
+ourApp.get('/', function(req, res){
+res.send(`
+<form action="/answer" method="POST">
+<h1>¿Cual es el color del cielo cuando no hay nuven?</h1>
+<input name="skyColor" autocomplete="off">
+<button>Enviar Respuesta</button>
+</form>
+
+`)
+})
+
+ourApp.post('/answer', function(req, res){
+res.send("Gracias por enviar una respuesta")
+})
+
+ourApp.get('/answer', function(req, res){
+    res.send("¿Estas perdido? no hay nada")
+})   
+
+ourApp.listen(3000)
