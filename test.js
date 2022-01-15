09b2a7418e4b7395901ce234284fbdc42ba8d2ef
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-15 05:10:16 -0300

    coments

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
 const requestListener = function (req, res) {
-  console.log(req.url)
+  console.log(req.url) // about
   res.writeHead(200);
-  res.end('Hello, World!');
+  res.end('Hello, World!'); // root
 }
 
 const server = http.createServer(requestListener);
