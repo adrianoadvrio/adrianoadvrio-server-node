8cd11aa620e1a728c2536c211bb4ae6ba3ac102c
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-15 15:56:04 -0300

    express module basic complete

--- express.js
+++ express.js
@@ -1,5 +1,8 @@
 let express = require("express")
 let ourApp = express()
+
+ourApp.use(express.urlencoded({extended:false}));
+
 ourApp.get('/', function(req, res){
 res.send(`
 <form action="/answer" method="POST">
@@ -12,7 +15,17 @@ res.send(`
 })
 
 ourApp.post('/answer', function(req, res){
-res.send("Gracias por enviar una respuesta")
+if(req.body.skyColor.toUpperCase() == "AZUL") {
+res.send(`
+<h2>Felicitaciones, la respuesta es corecta.</h2>
+<a href="/">Volver a la pagina principal</a>
+`)
+}else{
+    res.send(`
+    <h2>No es la respuesta corecta</h2>
+    <a href="/">Volver a la pagina principal</a>
+    `)
+}
 })
 
 ourApp.get('/answer', function(req, res){
